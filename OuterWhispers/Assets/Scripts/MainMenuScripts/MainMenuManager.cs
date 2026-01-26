b757f1aad81cfa7a8bb8b7a24e7f159b83bb67a7
MARTÍNEZ COMPTE <isamarcom@ti9-03.infor.caminas>
2026-01-26 20:41:39 +0100

    Añadido la escena para el menu y la interfaz.

---
+++ OuterWhispers/Assets/Scripts/MainMenuScripts/MainMenuManager.cs
@@ -0,0 +1,78 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.SceneManagement;
+using Application = UnityEngine.Application;
+
+public class MainMenuManager : MonoBehaviour
+{
+    private Button StartButton;
+    private Button QuitButton;
+    private Button OptionsButton;
+    public GameObject UiOptions;
+    public GameObject UiLoadMenu;
+
+    void Start()
+    {
+        
+    }
+    void OnEnable()
+    {
+        var uiDocument = GetComponent<UIDocument>();
+        var root = uiDocument.rootVisualElement;
+        StartButton = root.Q<Button>("StartButton");
+        QuitButton = root.Q<Button>("QuitButton");
+        OptionsButton = root.Q<Button>("SettingsButton");
+
+        if (StartButton != null) StartButton.clicked += OnStartClicked;
+        if (QuitButton != null) QuitButton.clicked += OnQuitClicked;
+        if (OptionsButton != null) OptionsButton.clicked += OnOptionsClicked;
+        StartButton.RegisterCallback<MouseEnterEvent>(OnHoverEnterStart);
+        StartButton.RegisterCallback<MouseLeaveEvent>(OnHoverExitStart);
+        QuitButton.RegisterCallback<MouseEnterEvent>(OnHoverEnterQuit);
+        QuitButton.RegisterCallback<MouseLeaveEvent>(OnHoverExitQuit);
+        OptionsButton.RegisterCallback<MouseEnterEvent>(OnHoverEnterOptions);
+        OptionsButton.RegisterCallback<MouseLeaveEvent>(OnHoverExitOptions);
+
+    }
+
+    private void OnStartClicked()
+    {
+        UiLoadMenu.SetActive(true);
+    }
+
+    private void OnQuitClicked()
+    {
+        Application.Quit();
+    }
+
+    private void OnOptionsClicked()
+    {
+        // Activa el UI Options
+        UiOptions.SetActive(true);
+
+    }
+    void OnHoverEnterStart(MouseEnterEvent evt)
+    {
+        StartButton.style.backgroundColor = new StyleColor(Color.grey);
+    }
+    void OnHoverExitStart(MouseLeaveEvent evt)
+    {
+        StartButton.style.backgroundColor = new StyleColor(Color.black);
+    }
+    void OnHoverEnterQuit(MouseEnterEvent evt)
+    {
+        QuitButton.style.backgroundColor = new StyleColor(Color.grey);
+    }
+    void OnHoverExitQuit(MouseLeaveEvent evt)
+    {
+        QuitButton.style.backgroundColor = new StyleColor(Color.black);
+    }
+    void OnHoverEnterOptions(MouseEnterEvent evt)
+    {
+        OptionsButton.style.backgroundColor = new StyleColor(Color.grey);
+    }
+    void OnHoverExitOptions(MouseLeaveEvent evt)
+    {
+        OptionsButton.style.backgroundColor = new StyleColor(Color.black);
+    }
+}
---
+++ OuterWhispers/Assets/Scripts/MainMenuScripts/OptionsMenuManager.cs
@@ -0,0 +1,75 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+public class OptionsMenuManager : MonoBehaviour
+{
+    private bool isOpen = false;
+    private Button CloseButton;
+    public GameObject UiOptions;
+    private SliderInt sliderSound;
+    private SliderInt sliderMusic;
+    void Start()
+    {
+        UiOptions.SetActive(false);
+    }
+    void OnEnable()
+    {
+        var uiDocument = GetComponent<UIDocument>();
+        var root = uiDocument.rootVisualElement;
+        CloseButton = root.Q<Button>("CloseButton");
+        sliderSound = root.Q<SliderInt>("SliderSound");
+        sliderMusic = root.Q<SliderInt>("SliderMusic");
+        if (CloseButton != null) CloseButton.clicked += OnCloseClicked;
+        
+        if (sliderSound != null)
+        {
+            sliderSound.RegisterValueChangedCallback(OnSliderSoundChanged);
+        }
+
+        if (sliderMusic != null)
+        {
+            sliderMusic.RegisterValueChangedCallback(OnSliderMusicChanged);
+        }
+
+        CloseButton.RegisterCallback<MouseEnterEvent>(OnHoverEnterClose);
+        CloseButton.RegisterCallback<MouseLeaveEvent>(OnHoverExitClose);
+
+    }
+    private void OnReturnClicked()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    void OnHoverEnterClose(MouseEnterEvent evt)
+    {
+        CloseButton.style.backgroundColor = new StyleColor(Color.grey);
+    }
+    void OnHoverExitClose(MouseLeaveEvent evt)
+    {
+        CloseButton.style.backgroundColor = new StyleColor(new Color(0, 0, 0, 0));
+    }
+    private void OnDisable()
+    {
+        if (sliderSound != null)
+            sliderSound.UnregisterValueChangedCallback(OnSliderSoundChanged);
+
+        if (sliderMusic != null)
+            sliderMusic.UnregisterValueChangedCallback(OnSliderMusicChanged);
+    }
+
+    private void OnSliderSoundChanged(ChangeEvent<int> evt)
+    {
+        float vol = evt.newValue / 100f;
+    }
+
+    private void OnSliderMusicChanged(ChangeEvent<int> evt)
+    {
+        float vol = evt.newValue / 100f;
+    }
+    private void OnCloseClicked()
+    {
+
+        UiOptions.SetActive(false);
+
+    }
+}
